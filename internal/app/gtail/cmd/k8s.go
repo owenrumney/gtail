63ed98f47963cbfcbe4d7ac06be5f56886b65e3c
Owen Rumney <owen@owenrumney.co.uk>
2023-01-30 19:37:26 +0000

    feat: update k8s log printing (#5)

--- internal/app/gtail/cmd/k8s.go
+++ internal/app/gtail/cmd/k8s.go
@@ -1,14 +1,20 @@
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
+	"time"
 
 	"cloud.google.com/go/logging"
 	"cloud.google.com/go/logging/apiv2/loggingpb"
 	"github.com/owenrumney/gtail/internal/pkg/logfilter"
 	"github.com/owenrumney/gtail/internal/pkg/logs"
+	"github.com/owenrumney/gtail/pkg/logger"
 	"github.com/owenrumney/gtail/pkg/output"
 	"github.com/spf13/cobra"
+	appEngine "google.golang.org/genproto/googleapis/appengine/logging/v1"
+	"google.golang.org/genproto/googleapis/cloud/audit"
+	"google.golang.org/protobuf/proto"
 )
 
 var k8sCommand = &cobra.Command{
@@ -48,11 +54,46 @@ func defaultK8sLogWriter(value interface{}) error {
 		case *loggingpb.LogEntry_TextPayload:
 			content = payload.TextPayload
 		case *loggingpb.LogEntry_ProtoPayload:
-			content = fmt.Sprintf("%v", payload.ProtoPayload)
+			switch payload.ProtoPayload.TypeUrl {
+			case "type.googleapis.com/google.cloud.audit.AuditLog":
+				var auditLog audit.AuditLog
+				err := proto.Unmarshal(payload.ProtoPayload.Value, &auditLog)
+				if err != nil {
+					logger.Error("error unmarshalling audit log: %v", err)
+					content = fmt.Sprintf("%v", payload.ProtoPayload)
+				} else {
+					contentBytes, err := json.MarshalIndent(auditLog, "", "  ")
+					if err != nil {
+						logger.Error("error marshalling audit log: %v", err)
+						content = fmt.Sprintf("%v", auditLog)
+					} else {
+						content = string(contentBytes)
+					}
+				}
+			case "type.googleapis.com/google.appengine.logging.v1.RequestLog":
+				var requestLog appEngine.RequestLog
+				err := proto.Unmarshal(payload.ProtoPayload.Value, &requestLog)
+				if err != nil {
+					logger.Error("error unmarshalling audit log: %v", err)
+					content = fmt.Sprintf("%v", payload.ProtoPayload)
+				} else {
+					content = fmt.Sprintf("%v", requestLog)
+				}
+			default:
+				content = fmt.Sprintf("%v", payload.ProtoPayload)
+			}
+
 		case *loggingpb.LogEntry_JsonPayload:
-			content = fmt.Sprintf("%v", payload.JsonPayload)
+			c, err := json.MarshalIndent(payload.JsonPayload, "", "  ")
+			if err != nil {
+				logger.Error("error marshalling json payload: %v", err)
+				content = fmt.Sprintf("%v", payload.JsonPayload)
+			} else {
+				content = string(c)
+			}
 		}
-		fmt.Printf("%v\n", content)
+
+		fmt.Printf("%s: %v\n", t.Timestamp.AsTime().Format(time.RFC3339), content)
 	default:
 		fmt.Printf("%v\n", value)
 	}
